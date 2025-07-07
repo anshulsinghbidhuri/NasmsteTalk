291f876f0f3fd41ee8e171662f9f641a44205d5b
Anshul Singh <anshulsinghbidhuri@gmail.com>
2025-07-07 18:40:04 +0530

    Some changes

--- backend/src/controllers/auth.controllers.js
+++
@@ -1,7 +0,0 @@
-export async function signup(req, res) {
-    res.send("Signup Router!");
-} export async function login(req, res) {
-    res.send("Logout Router!");
-} export function logout(req, res) {
-    res.send("Login Router!");
-}
