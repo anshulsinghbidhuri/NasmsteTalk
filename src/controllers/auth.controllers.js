774041dcca8ba07c3f14d5e04af2366d5a4c286f
Anshul Singh <anshulsinghbidhuri@gmail.com>
2025-07-07 18:18:52 +0530

    Db setup done

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,7 @@
+export async function signup(req, res) {
+    res.send("Signup Router!");
+} export async function login(req, res) {
+    res.send("Logout Router!");
+} export function logout(req, res) {
+    res.send("Login Router!");
+}
