67cdc85305681d3eb9c9da20ef3edfc2f8094ec0
Anshul Singh <anshulsinghbidhuri@gmail.com>
2025-08-04 22:48:52 +0530

    made a delete API for the DB

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { signup, login, logout, onBoarding, Delete } from "../controllers/auth.controllers.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
-import { signup, login, logout } from "../controllers/auth.controllers.js";
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/onBoarding", protectRoute, onBoarding);
+router.delete("/delete-user", protectRoute, Delete);
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import { connectDB } from "./lib/db.js";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 
 const app = express()
+app.use(express.json());
+app.use(cookieParser());
 const PORT = process.env.PORT
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 })
